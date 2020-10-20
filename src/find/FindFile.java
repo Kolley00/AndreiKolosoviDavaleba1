a7bc74e34b85717e2888721fecfe38ee16c53856
Andrei <andrei.kolosovi1@btu.edu.gededwdwdwqwdqjnbdqwd>
2020-10-20 21:03:17 +0400

    init

---
+++ src/find/FindFile.java
@@ -0,0 +1,40 @@
+package find;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class FindFile {
+
+    public static final Logger logger = Logger.getLogger(
+            FindFile.class.getName());
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the file search " );
+        String name_find = scan.next();
+        System.out.println("Enter the directory ");
+        String directory = scan.next();
+        File dir = new File(directory);
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept (File dir, String name) {
+                return name.contains(name_find);
+            }
+        };
+
+
+        String[] dir_list = dir.list(filter);
+        if (dir_list == null) {
+            System.out.println("Not found");
+        } else {
+            for (String filename : dir_list) {
+
+                logger.info(filename);
+            }
+        }
+    }
+
+}
